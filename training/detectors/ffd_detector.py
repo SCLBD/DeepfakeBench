80371880f557c2605dc6045686709b24bea4d77e
StevenYan <810824494@qq.com>
2023-07-07 10:28:07 +0800

    update

--- training/detectors/ffd_detector.py
+++ training/detectors/ffd_detector.py
@@ -251,7 +251,7 @@ class PCATemplateMap(nn.Module):
 def get_templates():
     templates_list = []
     for i in range(10):
-        img = imread('.lib/MCT/template{:d}.png'.format(i))
+        img = imread('./lib/component/MCT/template{:d}.png'.format(i))
         templates_list.append(transforms.functional.to_tensor(img)[0:1,0:19,0:19])
     if torch.cuda.is_available():
         templates = torch.stack(templates_list).cuda()
