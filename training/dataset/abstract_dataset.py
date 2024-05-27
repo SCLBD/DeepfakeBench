6e5c24011a82d0aa653d79107670c221dead737f
YAN, Zhiyuan <810824494@qq.com>
2024-05-27 11:54:44 +0800

    Update abstract_dataset.py

--- training/dataset/abstract_dataset.py
+++ training/dataset/abstract_dataset.py
@@ -201,9 +201,9 @@ class DeepfakeAbstractBaseDataset(data.Dataset):
                 frame_paths = video_info['frames']
                 # sorted video path to the lists
                 if '\\' in frame_paths[0]:
-                    frame_paths = sorted(frame_paths, key=lambda x: int(frame_paths[0].split('\\')[-1].split('.')[0]))
+                    frame_paths = sorted(frame_paths, key=lambda x: int(x.split('\\')[-1].split('.')[0]))
                 else:
-                    frame_paths = sorted(frame_paths, key=lambda x: int(frame_paths[0].split('/')[-1].split('.')[0]))
+                    frame_paths = sorted(frame_paths, key=lambda x: int(x.split('/')[-1].split('.')[0]))
 
                 # Consider the case when the actual number of frames (e.g., 270) is larger than the specified (i.e., self.frame_num=32)
                 # In this case, we select self.frame_num frames from the original 270 frames
