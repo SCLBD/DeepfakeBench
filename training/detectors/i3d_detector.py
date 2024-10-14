ad2d546983f6ba4545fcd535dc18ee98232e866d
KeithKangranZhao <2971175954@qq.com>
2024-10-14 14:54:02 +0800

    Update I3D detector
    Clean code format and update config file for training

--- training/detectors/i3d_detector.py
+++ training/detectors/i3d_detector.py
@@ -57,7 +57,6 @@ RNG_SEED: 0
 OUTPUT_DIR: .
 """
 
-
 '''
 # author: Zhiyuan Yan
 # email: zhiyuanyan@link.cuhk.edu.cn
@@ -85,31 +84,16 @@ Reference:
 }
 '''
 
-import os
-import datetime
 import logging
-import numpy as np
-from sklearn import metrics
-from typing import Union
-from collections import defaultdict
+import os
+import sys
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.nn import DataParallel
-from torch.utils.tensorboard import SummaryWriter
-
+from detectors import DETECTOR
+from loss import LOSSFUNC
 from metrics.base_metrics_class import calculate_metrics_for_train
 
 from .base_detector import AbstractDetector
-from detectors import DETECTOR
-from networks import BACKBONE
-from loss import LOSSFUNC
 
-
-import os
-import sys
 current_file_path = os.path.abspath(__file__)
 parent_dir = os.path.dirname(os.path.dirname(current_file_path))
 project_root_dir = os.path.dirname(parent_dir)
@@ -120,13 +104,12 @@ import torch
 from .utils.slowfast.models.video_model_builder import ResNet as ResNetOri
 from .utils.slowfast.config.defaults import get_cfg
 from torch import nn
-import random
-
 
 random_select = True
 no_time_pool = True
 
 logger = logging.getLogger(__name__)
+
 
 @DETECTOR.register_module(module_name='i3d')
 class I3DDetector(AbstractDetector):
@@ -137,7 +120,7 @@ class I3DDetector(AbstractDetector):
         cfg.NUM_GPUS = 1
         cfg.TEST.BATCH_SIZE = 1
         cfg.TRAIN.BATCH_SIZE = 1
-        cfg.DATA.NUM_FRAMES = 16
+        cfg.DATA.NUM_FRAMES = config['clip_size']
         self.resnet = ResNetOri(cfg)
         if config['pretrained'] is not None:
             print(f"loading pretrained model from {config['pretrained']}")
@@ -150,45 +133,44 @@ class I3DDetector(AbstractDetector):
             self.resnet.load_state_dict(modified_weights, strict=True)
 
         self.loss_func = nn.BCELoss()  # The output of the model is a probability value between 0 and 1 (haved used sigmoid)
-    
+
     def build_backbone(self, config):
         pass
-    
+
     def build_loss(self, config):
         # prepare the loss function
         loss_class = LOSSFUNC[config['loss_func']]
         loss_func = loss_class()
         return loss_func
-    
+
     def features(self, data_dict: dict) -> torch.tensor:
-        inputs = [data_dict['image'].permute(0,2,1,3,4)]
+        inputs = [data_dict['image'].permute(0, 2, 1, 3, 4)]
         pred = self.resnet(inputs)
-        output = {}
-        output["final_output"] = pred
+        output = {"final_output": pred}
+
         return output["final_output"]
 
     def classifier(self, features: torch.tensor):
         pass
-    
+
     def get_losses(self, data_dict: dict, pred_dict: dict) -> dict:
         label = data_dict['label'].float()
         pred = pred_dict['cls'].view(-1)
         loss = self.loss_func(pred, label)
         loss_dict = {'overall': loss}
+
         return loss_dict
-    
+
     def get_train_metrics(self, data_dict: dict, pred_dict: dict) -> dict:
         label = data_dict['label']
         pred = pred_dict['cls']
-        # compute metrics for batch data
         auc, eer, acc, ap = calculate_metrics_for_train(label.detach(), pred.detach())
         metric_batch_dict = {'acc': acc, 'auc': auc, 'eer': eer, 'ap': ap}
+
         return metric_batch_dict
 
     def forward(self, data_dict: dict, inference=False) -> dict:
-        # get the probability
         prob = self.features(data_dict)
-        # build the prediction dict for each output
         pred_dict = {'cls': prob, 'prob': prob, 'feat': prob}
-        
+
         return pred_dict
