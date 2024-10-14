f06568b620b330286284a191a1b3d9a68a5e469a
KeithKangranZhao <2971175954@qq.com>
2024-10-14 14:58:52 +0800

    Update X-CLIP detector
    1. Clean code format.
    2. Update config file for training.

--- training/detectors/xclip_detector.py
+++ training/detectors/xclip_detector.py
@@ -1,4 +1,4 @@
-'''
+"""
 # author: Zhiyuan Yan
 # email: zhiyuanyan@link.cuhk.edu.cn
 # date: 2023-0706
@@ -23,33 +23,20 @@ Reference:
   pages={638--647},
   year={2022}
 }
-'''
+"""
 
-import os
-import datetime
 import logging
-import numpy as np
-from sklearn import metrics
-from typing import Union
-from collections import defaultdict
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torch.nn import DataParallel
-from torch.utils.tensorboard import SummaryWriter
-
+from detectors import DETECTOR
+from loss import LOSSFUNC
 from metrics.base_metrics_class import calculate_metrics_for_train
 
 from .base_detector import AbstractDetector
-from detectors import DETECTOR
-from networks import BACKBONE
-from loss import LOSSFUNC
-
-import loralib as lora
 
 logger = logging.getLogger(__name__)
+
 
 @DETECTOR.register_module(module_name='xclip')
 class XCLIPDetector(AbstractDetector):
@@ -64,19 +51,20 @@ class XCLIPDetector(AbstractDetector):
 
     def build_backbone(self, config):
         from transformers import XCLIPVisionModel
-        backbone = XCLIPVisionModel.from_pretrained("microsoft/xclip-base-patch32")
+        backbone = XCLIPVisionModel.from_pretrained(config['pretrained'])
+
         return backbone
-    
+
     def build_temporal_module(self, config):
-        model = nn.LSTM(input_size=2048, hidden_size=512, num_layers=3, batch_first=True)
-        return model
-    
+        return nn.LSTM(input_size=2048, hidden_size=512, num_layers=3, batch_first=True)
+
     def build_loss(self, config):
         # prepare the loss function
         loss_class = LOSSFUNC[config['loss_func']]
         loss_func = loss_class()
+
         return loss_func
-    
+
     def features(self, data_dict: dict) -> torch.tensor:
         # b, t, c, h, w = data_dict['image'].shape
         # frame_input = data_dict['image'].reshape(-1, c, h, w)
@@ -91,57 +79,33 @@ class XCLIPDetector(AbstractDetector):
         outputs = self.backbone(pixel_values, output_hidden_states=True)
         sequence_output = outputs['pooler_output'].reshape(batch_size, num_frames, -1)
         video_level_features = self.fc_norm(sequence_output.mean(1))
+
         return video_level_features
 
     def classifier(self, features: torch.tensor) -> torch.tensor:
         return self.head(features)
-    
+
     def get_losses(self, data_dict: dict, pred_dict: dict) -> dict:
         label = data_dict['label']
         pred = pred_dict['cls']
         loss = self.loss_func(pred, label)
         loss_dict = {'overall': loss}
+
         return loss_dict
-    
+
     def get_train_metrics(self, data_dict: dict, pred_dict: dict) -> dict:
         label = data_dict['label']
         pred = pred_dict['cls']
-        # compute metrics for batch data
-        # auc, eer, acc, ap = calculate_metrics_for_train(label.detach(), pred.detach())
-        # metric_batch_dict = {'acc': acc, 'auc': auc, 'eer': eer, 'ap': ap}
-        metric_batch_dict = {'acc': 0, 'auc': 0, 'eer': 0, 'ap': 0}
-        # we dont compute the video-level metrics for training
-        self.video_names = []
+        auc, eer, acc, ap = calculate_metrics_for_train(label.detach(), pred.detach())
+        metric_batch_dict = {'acc': acc, 'auc': auc, 'eer': eer, 'ap': ap}
+
         return metric_batch_dict
 
     def forward(self, data_dict: dict, inference=False) -> dict:
-        # get the features by backbone
         features = self.features(data_dict)
-        # get the prediction by classifier
         pred = self.classifier(features)
-        # get the probability of the pred
         prob = torch.softmax(pred, dim=1)[:, 1]
-        # build the prediction dict for each output
         pred_dict = {'cls': pred, 'prob': prob, 'feat': features}
-        # if inference:
-        #     self.prob.extend(
-        #         pred_dict['prob']
-        #         .detach()
-        #         .squeeze()
-        #         .cpu()
-        #         .numpy()
-        #     )
-        #     self.label.extend(
-        #         data_dict['label']
-        #         .detach()
-        #         .squeeze()
-        #         .cpu()
-        #         .numpy()
-        #     )
-        #     # deal with acc
-        #     _, prediction_class = torch.max(pred, 1)
-        #     correct = (prediction_class == data_dict['label']).sum().item()
-        #     self.correct += correct
-        #     self.total += data_dict['label'].size(0)
+
 
         return pred_dict
