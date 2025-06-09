b36e24a386e6802fbeeea1bd1a4c62d90399dbd4
zhaokangran <zhaokangran@versee.studio>
2025-06-09 13:32:40 +0800

    [Update] Remove Redundant Files

--- training/config/config/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-current_file_path = os.path.abspath(__file__)
-parent_dir = os.path.dirname(os.path.dirname(current_file_path))
-project_root_dir = os.path.dirname(parent_dir)
-sys.path.append(parent_dir)
-sys.path.append(project_root_dir)
